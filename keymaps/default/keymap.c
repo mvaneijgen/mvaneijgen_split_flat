781aff0524995148271f8c6a298b62f1cfb76888
Mitchel van Eijgen <mvaneijgen@gmail.com>
2021-06-20 12:59:16 +0200

    add custom short cuts

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -32,23 +32,27 @@
 #define ALT_RCBR LALT_T(KC_F22)
 #define CTL_SCLN RCTL_T(KC_F22)
 // END 🏠️ Home row mods -------------------------------------//
-
-// enum custom_keycodes {
-//     KC_BSPCDEL,
-// };
+//------------------------------------------------------//
+// 👨‍💻 Custom short codes  
+//------------------------------------------------------//
+#define KC_CAPW LGUI(LSFT(KC_3)) // Capture whole screen
+#define KC_CAPP LGUI(LSFT(KC_4)) // Capture portion of screen
+#define KC_TLFT LGUI(LSFT(KC_LBRC)) // Move tab left
+#define KC_TRGT LGUI(LSFT(KC_RBRC)) // Move tab Rigth
+// END 👨‍💻 Custom short codes  -------------------------------------//
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     LT(_RESET, KC_ESC), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, 
     KC_TAB, CTL_A, ALT_S, GUI_D, SFT_F, KC_G, KC_H, SFT_J, GUI_K, ALT_L, CTL_BSLS, KC_EQL,
     KC_GRV, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_EQL, 
-    RESET, MO(_NUMSYM), KC_ENT, KC_DEL, KC_BSPC, KC_SPC, MO(_CTRL), RESET
+    MO(_NUMSYM), MO(_NUMSYM), KC_ENT, KC_DEL, KC_BSPC, KC_SPC, MO(_CTRL), MO(_CTRL)
   ),
   [_NUMSYM] = LAYOUT(
     KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS, 
-    KC_CAPS, CTL_COLN, ALT_LCBR, GUI_LPRN, SFT_LBRC, KC_QUOT, KC_DQT, SFT_RBRC, GUI_RPRN, ALT_RCBR, CTL_SCLN, KC_EQL,
-    KC_TRNS, KC_TILD, KC_PIPE, KC_AT, KC_MINS, KC_DLR, KC_LT, KC_GT, KC_AMPR, KC_PLUS, KC_UNDS, KC_QUES, 
-    KC_TRNS, KC_TRNS, TG(_NUMSYM), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_TAB, CTL_COLN, ALT_LCBR, GUI_LPRN, SFT_LBRC, KC_QUOT, KC_DQT, SFT_RBRC, GUI_RPRN, ALT_RCBR, CTL_SCLN, KC_EQL,
+    KC_TLFT, KC_TILD, KC_PIPE, KC_AT, KC_MINS, KC_DLR, KC_LT, KC_GT, KC_AMPR, KC_PLUS, KC_UNDS, KC_QUES, 
+    KC_TRGT, KC_TRNS, TG(_NUMSYM), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
   [_CTRL] = LAYOUT(
     KC_TRNS, KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MFFD, KC_TRNS, KC_TRNS, KC_TRNS, 
